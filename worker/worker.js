02420b89390c0f9eb62ab5f6814f0364e5ad2096
tomek7667 <38402753+tomek7667@users.noreply.github.com>
2024-08-21 23:26:12 +0200

    Initial commit

---
+++ worker/worker.js
@@ -0,0 +1,12 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const main = async () => {
+	console.log(`${new Date().toLocaleString()} | worker loaded`);
+};
+
+module.exports = { main };
+
+if (process.argv.some((a) => a.includes("worker.js"))) {
+	main();
+}
