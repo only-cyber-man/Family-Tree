3b64a40168658d78c3f1bcb1b9b0fa1b57aae667
tomek7667 <git@cyber-man.pl>
2024-08-21 23:34:52 +0200

    Prepare for deployment

--- cyber-man-family-tree.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Preparing Family Tree..."
-PROJECT_PATH="/home/ubuntu/services/family-tree"
-echo "Creating $PROJECT_PATH"
-mkdir -p $PROJECT_PATH
-echo "console.log('init Family Tree');" > $PROJECT_PATH/server.js
-pm2 start $PROJECT_PATH/server.js --name "Family Tree"
